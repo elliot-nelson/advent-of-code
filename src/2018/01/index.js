a532284ead8bf4101d29bfd3a03795dbdb0309ca
Elliot Nelson <Elliot.Nelson@hbo.com>
2019-07-27 13:56:38 -0400

    :christmas_tree: Year 2018 Day 01

---
+++ src/2018/01/index.js
@@ -0,0 +1,31 @@
+// ------------------------------------------------------------------------
+// 2018/01
+// ------------------------------------------------------------------------
+const { log } = require('../../util');
+
+module.exports = function solve(lines) {
+  //// Part 1 /////
+  let freq = 0;
+  for (let line of lines) {
+    freq += parseInt(line, 10);
+  }
+  let result1 = freq;
+  log(`Part 1: ${result1}`);
+
+  //// Part 2 ////
+  freq = 0;
+  let hash = {};
+  let found;
+  for (;found === undefined;) {
+    for (let line of lines) {
+      freq += parseInt(line, 10);
+      hash[freq] = (hash[freq]||0) + 1;
+      if (hash[freq] > 1) {
+        found = freq;
+        break;
+      }
+    }
+  }
+  let result2 = freq;
+  log(`Part 2: ${result2}`);
+};
