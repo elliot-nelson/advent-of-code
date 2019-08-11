60df1e4fcf922118e7af3ec90db09562a06e8d54
Elliot Nelson <Elliot.Nelson@hbo.com>
2019-08-11 10:55:13 -0400

    :christmas_tree: Year 2017 Day 09

---
+++ src/2017/09/index.ts
@@ -0,0 +1,78 @@
+// ------------------------------------------------------------------------
+// Year 2017 Day 09
+// ------------------------------------------------------------------------
+import { log } from '../../util';
+
+function parse(input) {
+  let state = 'group';
+  let stack: any[] = [[]];
+
+  for (let c of input) {
+    let top = stack[stack.length - 1];
+
+    switch (state) {
+      case 'group':
+        if (c === '{') {
+          let group = [];
+          top.push(group);
+          stack.push(group);
+        } else if (c === '}') {
+          stack.pop();
+        } else if (c === '<') {
+          state = 'garbage';
+          stack.push('');
+        }
+        break;
+      case 'garbage':
+        if (c === '>') {
+          stack.pop();
+          stack[stack.length - 1].push(top);
+          state = 'group';
+        } else if (c === '!') {
+          state = 'cancel';
+        } else {
+          stack[stack.length - 1] += c;
+        }
+        break;
+      case 'cancel':
+        state = 'garbage';
+        break;
+    }
+  }
+
+  return stack[0][0];
+}
+
+function computeScore(group, level) {
+  let score = level;
+  for (let subgroup of group) {
+    if (Array.isArray(subgroup)) {
+      score += computeScore(subgroup, level + 1);
+    }
+  }
+  return score;
+}
+
+function computeGarbageChars(group) {
+  let chars = 0;
+  for (let subgroup of group) {
+    if (Array.isArray(subgroup)) {
+      chars += computeGarbageChars(subgroup);
+    } else {
+      chars += subgroup.length;
+    }
+  }
+  return chars;
+}
+
+export function solve(input: string[]) {
+  //// Part 1 ////
+  let groups = parse(input[0]);
+  let result1 = computeScore(groups, 1);
+  console.log(JSON.stringify(result1));
+  log.part1(result1);
+
+  //// Part 2 ////
+  let result2 = computeGarbageChars(groups);
+  log.part2(result2);
+};
