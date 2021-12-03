100762e38c69198f71106c11a1f2667c9c709b1f
Elliot Nelson <nelson.work@gmail.com>
2021-12-03 06:45:42 -0500

    :sparkles: use bitwise operations instead of string operations

--- src/2021/03/BinaryDiagnostic.ts
+++ src/2021/03/BinaryDiagnostic.ts
@@ -3,42 +3,42 @@
 
 import { ISolution } from '../../util';
 
-function filterOnBit(list: string[], bit: number, highest: boolean): string[] {
-    let counts = { '0': 0, '1': 0 };
-    for (let i = 0; i < list.length; i++) counts[list[i][bit]]++;
+function filterOnBit(list: number[], bit: number, highest: boolean): number[] {
+    let counts = [0, 0];
+    for (let i = 0; i < list.length; i++) counts[(list[i] >> bit) & 1]++;
 
-    let criteria = counts['0'] > counts['1'] ? (highest ? '0' : '1') : (highest ? '1' : '0');
-    return list.filter(entry => entry[bit] === criteria);
+    let criteria: number = counts[0] > counts[1] ? (highest ? 0 : 1) : (highest ? 1 : 0);
+    return list.filter(entry => ((entry >> bit) & 1) === criteria);
 }
 
 export function solve(input: string[]): ISolution<number> {
+    const bits = input[0].length;
+    const numbers = input.map(line => parseInt(line, 2));
 
     // Part 1
-    let gammaString = '';
-    let epsilonString = '';
-    for (let bit = 0; bit < input[0].length; bit++) {
-        let counts = { '0': 0, '1': 0 };
-        for (let i = 0; i < input.length; i++) counts[input[i][bit]]++;
-        gammaString += counts['0'] > counts['1'] ? '0' : '1';
-        epsilonString += counts['0'] > counts['1'] ? '1' : '0';
+    let gamma: number = 0;
+    let epsilon: number = 0;
+    for (let bit = bits - 1; bit >= 0; bit--) {
+        let counts = [0, 0];
+        for (let i = 0; i < input.length; i++) counts[(numbers[i] >> bit) & 1]++;
+        gamma += (counts[0] > counts[1] ? 0 : 1) << bit;
+        epsilon += (counts[0] > counts[1] ? 1 : 0) << bit;
     }
-    let gamma = parseInt(gammaString, 2);
-    let epsilon = parseInt(epsilonString, 2);
 
     const part1 = gamma * epsilon;
 
     // Part 2
-    let oxygenList = [...input];
-    for (let bit = 0; oxygenList.length > 1; bit++) {
+    let oxygenList = [...numbers];
+    for (let bit = bits - 1; oxygenList.length > 1; bit--) {
         oxygenList = filterOnBit(oxygenList, bit, true);
     }
-    let oxygen = parseInt(oxygenList[0], 2);
+    let oxygen = oxygenList[0];
 
-    let co2List = [...input];
-    for (let bit = 0; co2List.length > 1; bit++) {
+    let co2List = [...numbers];
+    for (let bit = bits - 1; co2List.length > 1; bit--) {
         co2List = filterOnBit(co2List, bit, false);
     }
-    let co2 = parseInt(co2List[0], 2);
+    let co2 = co2List[0];
 
     const part2 = oxygen * co2;
 
