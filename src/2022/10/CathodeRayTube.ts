0aa3cdf3e5ffdfa2a2a33b899daf562374ca1c95
Elliot Nelson <nelson.work@gmail.com>
2022-12-10 17:07:13 -0500

    :bug: cleanup

--- src/2022/10/CathodeRayTube.ts
+++ src/2022/10/CathodeRayTube.ts
@@ -59,82 +59,17 @@ export function solve(input: string[]): ISolution<string> {
 
     const results: Result[] = simulate(instructions);
 
-
-
-/*
-    const output: string[][] = [
-        '........................................'.split(''),
-        '........................................'.split(''),
-        '........................................'.split(''),
-        '........................................'.split(''),
-        '........................................'.split(''),
-        '........................................'.split(''),
-    ];
-
     // Part 1
-    //console.log('part1');
-    for (;;) {
-        next--;
-        cycle++;
-            if (pending[cycle]) {
-                x += pending[cycle];
-                delete pending[cycle];
-            }
-        if (cycle === 20 || (cycle - 20) % 40 === 0) {
-            console.log(cycle, x, cycle * x);
-            sum += cycle * x;
-        }
-
-        // draw
-        const yval = Math.floor(((cycle - 1) % 240) / 40);
-        const xval = (cycle-1) % 40;
-        if (x >= xval - 1 && x <= xval + 1) {
-            output[yval][xval] = '#';
-        }
-
-        if (next <= 0) {
-            const instr = instrs.shift();
-            if (!instr) break;
-
-            switch (instr.i) {
-                case 'noop':
-                    next = 1;
-                    break;
-                case 'addx':
-                    next = 2;
-                    pending[cycle + 2] = instr.v;
-                    break;
-            }
-        }
-
-        if (instrs.length === 0) break;
-    }
-
-    console.log(sum);
-    const part1 = 0;
-
-    console.log(output.map(line => line.join('')).join('\n'));
-    // Part 2
-    //console.log('part2');
-    const part2 = 0;
-    */
-
     const part1 = results
         .filter(result => result.cycle === 20 || (result.cycle - 20) % 40 === 0)
         .map(result => result.cycle * result.x)
         .reduce((sum, value) => sum + value).toString();
 
-    const output: string[][] = [
-        '........................................'.split(''),
-        '........................................'.split(''),
-        '........................................'.split(''),
-        '........................................'.split(''),
-        '........................................'.split(''),
-        '........................................'.split(''),
-    ];
+    // Part 2
+    const output: string[][] = Array(6).fill(undefined).map(x => '.'.repeat(40).split(''));
     for (const result of results) {
         const yval = Math.floor(((result.cycle - 1) % 240) / 40);
-        const xval = (result.cycle-1) % 40;
+        const xval = (result.cycle - 1) % 40;
         if (result.x >= xval - 1 && result.x <= xval + 1) {
             output[yval][xval] = '#';
         }
